ed4a6a65bdd2768f777666f58370c4673bb632d1
federicoruffa <federico@inboundav.com>
2024-08-07 20:33:14 -0300

    add loader and favicon

--- components/ReservationForm.js
+++ components/ReservationForm.js
@@ -1,32 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import styles from './ReservationForm.module.css'; // Assuming you have a CSS module for styling
-import { supabase } from '../supabaseClient'; // Adjust the path if necessary
+import React, { useState, useEffect } from "react";
+import styles from "./ReservationForm.module.css"; // Assuming you have a CSS module for styling
+import { supabase } from "../supabaseClient"; // Adjust the path if necessary
 
 const TOTAL_PLACES = 40;
 
 const ReservationForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
+    name: "",
+    email: "",
     persons: 1,
   });
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const [remainingPlaces, setRemainingPlaces] = useState(TOTAL_PLACES);
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchTotalPersons = async () => {
+      setIsLoading(true);
       const { data, error } = await supabase
-        .from('reservations')
-        .select('persons');
+        .from("reservations")
+        .select("persons");
 
       if (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       } else {
-        const totalPersons = data.reduce((sum, reservation) => sum + reservation.persons, 0);
+        const totalPersons = data.reduce(
+          (sum, reservation) => sum + reservation.persons,
+          0
+        );
         const availablePlaces = TOTAL_PLACES - totalPersons;
         setRemainingPlaces(TOTAL_PLACES - totalPersons);
+        setIsLoading(false);
         if (availablePlaces <= 0) {
-          setMessage('No hay lugares disponibles.');
+          setMessage("No hay lugares disponibles.");
         }
       }
     };
@@ -46,97 +52,110 @@ const ReservationForm = () => {
     e.preventDefault();
     // Insert form data into Supabase
     const { data, error } = await supabase
-      .from('reservations')
+      .from("reservations")
       .insert([
-        { name: formData.name, email: formData.email, persons: formData.persons },
+        {
+          name: formData.name,
+          email: formData.email,
+          persons: formData.persons,
+        },
       ]);
 
     if (error) {
-      console.error('Error inserting data:', error);
-      setMessage(`Hubo un error al hacer la reserva. Por favor intenta de nuevo. Si el problema persiste, contactanos a @raro_duo en IG.`);
+      console.error("Error inserting data:", error);
+      setMessage(
+        `Hubo un error al hacer la reserva. Por favor intenta de nuevo. Si el problema persiste, contactanos a @raro_duo en IG.`
+      );
     } else {
-      console.log('Data inserted successfully:', data);
-      setMessage('La reserva se hizo con éxito! Te esperamos');
-      setRemainingPlaces(prev => prev - formData.persons);
+      console.log("Data inserted successfully:", data);
+      setMessage("La reserva se hizo con éxito! Te esperamos");
+      setRemainingPlaces((prev) => prev - formData.persons);
       setFormData({
-        name: '',
-        email: '',
+        name: "",
+        email: "",
         persons: 1,
       });
 
-     // Send confirmation email
+      // Send confirmation email
       try {
-        const response = await fetch('/api/sendEmail', {
-          method: 'POST',
+        const response = await fetch("/api/sendEmail", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(formData),
         });
 
         if (response.ok) {
-          console.log('Confirmation email sent successfully');
+          console.log("Confirmation email sent successfully");
         } else {
-          console.error('Error sending confirmation email', {
+          console.error("Error sending confirmation email", {
             status: response.status,
             statusText: response.statusText,
             url: response.url,
             headers: response.headers,
-            body: await response.text() // Assuming response is a Fetch API Response object
+            body: await response.text(), // Assuming response is a Fetch API Response object
           });
         }
       } catch (emailError) {
-        console.error('Error sending confirmation email:', emailError);
+        console.error("Error sending confirmation email:", emailError);
       }
     }
   };
 
   return (
     <>
-    <h5 className={styles.remainingPlaces}>Lugares disponibles: {remainingPlaces}</h5>
-    {message && <p className={styles.message}>{message}</p>}
-    <form className={styles.form} onSubmit={handleSubmit}>
-
-      <div className={styles.formGroup}>
-        <label htmlFor="name">Nombre y Apellido:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
+      <h5 className={styles.remainingPlaces}>
+        {isLoading ? "Cargando..." : `Lugares disponibles: ${remainingPlaces}`}
+      </h5>{" "}
+      {message && <p className={styles.message}>{message}</p>}
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name">Nombre y Apellido:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            disabled={remainingPlaces <= 0}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            disabled={remainingPlaces <= 0}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="persons">Cantidad (max 4):</label>
+          <input
+            type="number"
+            id="persons"
+            name="persons"
+            value={formData.persons}
+            onChange={handleChange}
+            min="1"
+            max="4"
+            required
+            disabled={remainingPlaces <= 0}
+          />
+        </div>
+        <button
+          type="submit"
+          className={styles.button}
           disabled={remainingPlaces <= 0}
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          disabled={remainingPlaces <= 0}
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="persons">Cantidad (max 4):</label>
-        <input
-          type="number"
-          id="persons"
-          name="persons"
-          value={formData.persons}
-          onChange={handleChange}
-          min="1"
-          max="4"
-          required
-          disabled={remainingPlaces <= 0}
-        />
-      </div>
-      <button type="submit" className={styles.button} disabled={remainingPlaces <= 0}>Reservar</button>
-    </form>
+        >
+          Reservar
+        </button>
+      </form>
     </>
   );
 };
--- pages/reservas.js
+++ pages/reservas.js
@@ -1,14 +1,22 @@
 import ReservationForm from "../components/ReservationForm";
 import globals from "../src/app/globals.css";
 import styles from './reservas.module.css';
+import Head from 'next/head'; // Import Head component
 
 export default function Reservas() {
   return (
+    <>
+    <Head>
+      <title>Raro | Reservas</title>
+      <meta name="description" content="Reserva tu lugar para el show de Raro" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <main className={`${styles.main} container`}>
       <h1 className={`${styles.title}`}>Reservas</h1>
       <p className={styles.text}>El show será a la gorra con reserva. Ingresá tus datos abajo para reservar.</p>
       <ReservationForm />
     </main>
+    </>
   );
 }
 
