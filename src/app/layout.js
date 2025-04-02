9b8611eeb75e6f74f61df41dc2810eb8621a4510
federicoruffa <federico@inboundav.com>
2025-04-02 12:32:37 -0300

    fix tag for real

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import styles from "./page.module.css";
+import Script from "next/script";
 
 const inter = Inter({
   weight: ["400", "700", "900"],
@@ -15,6 +16,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        strategy="beforeInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16582830774"
+      ></Script>
+      <Script id="google-analytics" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16582830774');
+        `}
+      </Script>
       <body className={inter.className}>
         <main className="main">{children}</main>
       </body>
